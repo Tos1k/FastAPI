85d912de7b88ef5b7a6a54534f08f16280a7e9de
Tos1k <2001ghghgh@gmail.com>
2024-07-21 14:53:25 +0400

    first

--- alembic/env.py
+++ alembic/env.py
@@ -1,8 +1,8 @@
 from logging.config import fileConfig
-
-from sqlalchemy import engine_from_config
+from models.models import metadata
+from sqlalchemy import engine_from_config, MetaData
 from sqlalchemy import pool
-from models.first import Base
+
 from alembic import context
 
 # this is the Alembic Config object, which provides
@@ -18,7 +18,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- models/models.py
+++ models/models.py
@@ -1,8 +1,9 @@
 from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime,timezone
-from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON
+from datetime import datetime, timezone
+from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON, Boolean
 
 Base = declarative_base()
+metadata = MetaData()
 
 
 class Role(Base):
@@ -20,5 +21,9 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    role_id = Column(Integer, ForeignKey(Role.id))
     registered_at = Column(TIMESTAMP, default=datetime.now(timezone.utc))
+    is_active: bool = Column(Boolean, default=True, nullable=False)
+    is_superuser: bool = Column(Boolean, default=False, nullable=False)
+    is_verified: bool = Column(Boolean, default=False, nullable=False)
